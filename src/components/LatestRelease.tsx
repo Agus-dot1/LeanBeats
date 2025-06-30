8e83eb50cae9dbb2c6c6551751229e3258ad89ce
Agustin de Oliveira <113210412+Agus-dot1@users.noreply.github.com>
2025-06-29 22:38:46 -0300

    Updated LatestRelease.tsx

--- src/components/LatestRelease.tsx
+++ src/components/LatestRelease.tsx
@@ -19,7 +19,7 @@ interface LatestReleaseProps {
 
 const defaultRelease: Release = {
   id: 'retumba-la-bocina',
-  title: "LA PRE$IÓN",
+  title: "LA PRE$IÓN 危 Vol • 1",
   artist: "Lea in the Mix",
   videoUrl: "https://res.cloudinary.com/do17gdc0b/image/upload/v1751131606/Bio_2_tfdjge.webp",
   youtubeUrl: "https://www.youtube.com/watch?v=KcdNDoIfGbs",
